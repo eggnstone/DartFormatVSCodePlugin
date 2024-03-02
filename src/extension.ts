87a9e728c18c4dedf7b23fea795b313cd1b6edb4
eggnstone <eggnstone@users.noreply.github.com>
2024-03-02 12:29:38 +0700

    Adding keybindings.

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,11 @@ export function activate(context: vscode.ExtensionContext)
 {
     //console.log('Congratulations, your extension "DartFormat" is now active!');
 
-    let disposable = vscode.commands.registerCommand('DartFormat.helloWorld', () =>
+    let disposable = vscode.commands.registerCommand('DartFormat.format', () =>
     {
         // noinspection JSIgnoredPromiseFromCall
-        vscode.window.showInformationMessage('Hello World from DartFormat!\nComing soon to format your Dart files :)');
+        vscode.window.showInformationMessage('Hello World from DartFormat!<br/>' +
+            'Coming soon to format your Dart files :)');
     });
 
     context.subscriptions.push(disposable);
